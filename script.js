216c214558add7f9f93a2750531459d4db5a282a
Isabela Biramontes <ibiramontes.student@hsfi.us>
2017-03-21 21:16:11 +0000

    I made a random generator

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
-
-
+var ScriptEders = ["Gabby","Anahi","Felix","Lauren","Pamela","Daril"];
+var ScriptEducators = ["Nick", "Matthew", "Jenna", "Rafi" , "Michael"];
+var studentrandom =  Math.floor(Math.random() * ScriptEders.length);
+var teacherrandom =  Math.floor(Math.random() * ScriptEducators.length);
+$("#studentButton").click(function(){
+    $("#studentDisplay").html(ScriptEders[studentrandom]);
 });
+
+$("#teacherButton").click(function(){
+    $("#teacherDisplay").html(ScriptEducators[teacherrandom]);
+});
+
+
+    
+    
+    });
+/* ScriptEders.push("Nicole");
+ScriptEducators.push("Bob");
+console.log(ScriptEducators[teacherrandom]);
+console.log(ScriptEders[studentrandom]); */
+
